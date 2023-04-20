cb6bab6249e49f37664376972d9177025e7a3adf
Luan Lemos <ipsluan@icloud.com>
2023-04-20 17:25:36 -0300

    remove theme white

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Luan Lemos | Desenvolvedor front-end</title>
+        <meta
+          name="description"
+          content="Portfólio de Luan Lemos, desenvolvedor front-end"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
